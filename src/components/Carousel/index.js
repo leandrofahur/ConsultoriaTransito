6727120f1c8d62a72f0e275bc356e3d156e7b98e
leandrofahur <fahurleandro@gmail.com>
2021-03-16 01:50:48 -0700

    bug fix on header - active button not working

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./index.css";
 
 const images = [
@@ -11,10 +12,9 @@ const Carousel = () => {
   const [current, setCurrent] = useState(0);
 
   useEffect(() => {
-    console.log(current);
     const id = setTimeout(() => {
       setCurrent((current + 1) % images.length);
-    }, 1500);
+    }, 2100);
     return () => {
       clearInterval(id);
     };
@@ -23,7 +23,20 @@ const Carousel = () => {
   const imageList = images.map((image) => {
     return (
       <div key={image}>
-        <img className="ui fluid image" src={image} key={image}></img>
+        <img
+          className="ui fluid image kenburns-top layer"
+          src={image}
+          key={image}
+        ></img>
+        {/* <div className="label link pos">
+          Photo by{" "}
+          <Link
+            style={{ color: "yellow", textDecoration: "underline" }}
+            to="https://unsplash.com/@danielmonteirox?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+          >
+            Daniel Monteiro{" "}
+          </Link>{" "}
+        </div> */}
       </div>
     );
   });
--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -9,7 +9,75 @@ const Homepage = () => {
       <Header />
       <Carousel />
       <main>
-        <div className="ui container"></div>
+        <div className="ui inverted message attached">
+          <div className="ui container">
+            <div class="ui link cards">
+              <div class="card">
+                <div class="image">
+                  <img src="/images/avatar2/large/matthew.png" />
+                </div>
+                <div class="content">
+                  <div class="header">Matt Giampietro</div>
+                  <div class="meta">
+                    <a>Friends</a>
+                  </div>
+                  <div class="description">
+                    Matthew is an interior designer living in New York.
+                  </div>
+                </div>
+                <div class="extra content">
+                  <span class="right floated">Joined in 2013</span>
+                  <span>
+                    <i class="user icon"></i>
+                    75 Friends
+                  </span>
+                </div>
+              </div>
+              <div class="card">
+                <div class="image">
+                  <img src="/images/avatar2/large/molly.png" />
+                </div>
+                <div class="content">
+                  <div class="header">Molly</div>
+                  <div class="meta">
+                    <span class="date">Coworker</span>
+                  </div>
+                  <div class="description">
+                    Molly is a personal assistant living in Paris.
+                  </div>
+                </div>
+                <div class="extra content">
+                  <span class="right floated">Joined in 2011</span>
+                  <span>
+                    <i class="user icon"></i>
+                    35 Friends
+                  </span>
+                </div>
+              </div>
+              <div class="card">
+                <div class="image">
+                  <img src="/images/avatar2/large/elyse.png" />
+                </div>
+                <div class="content">
+                  <div class="header">Elyse</div>
+                  <div class="meta">
+                    <a>Coworker</a>
+                  </div>
+                  <div class="description">
+                    Elyse is a copywriter working in New York.
+                  </div>
+                </div>
+                <div class="extra content">
+                  <span class="right floated">Joined in 2014</span>
+                  <span>
+                    <i class="user icon"></i>
+                    151 Friends
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
